578af271cb4986d4de19a941c275494c5fa1dc6c
kenkimata <21082@students.mrgs.school.nz>
2024-05-15 14:44:46 +1200

    Small changes

--- Main.py
+++ Main.py
@@ -1,7 +1,22 @@
 from tkinter import *
 
+w_width = 500
+w_height = 700
+
 window = Tk()
-window.geometry("500x300")
+window.geometry(str(w_width) + "x" + str(w_height))
 window.title("VisionPass")
+
+main_frame = Frame(background="black", width=w_width, height=w_height)
+main_frame.pack()
+
+hello_label = Label(text="Home")
+hello_label.place(x=10, y=10)
+
+hello_label = Label(text="Passwords")
+hello_label.place(x=60, y=10)
+
+hello_label = Label(text="Generator")
+hello_label.place(x=130, y=10)
 
 window.mainloop()
